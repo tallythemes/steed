a3aef0139b69e60b5988acfb2b56de3890f7a74e
TallyThemes <dev@tallythemes.com>
2017-01-26 17:02:01 +0600

    Update Page builder template

--- template-page-bulder.php
+++ template-page-bulder.php
@@ -10,8 +10,7 @@
 ?>
 <?php get_header(); ?>
 	<?php steed_site_header(); ?>
-    <?php steed_before_site_content(array('in_class' => 'container-width no-sidebar')); ?>
-
+    <?php steed_before_site_content(array('in_class' => 'container-width no-sidebar', 'class' => 'full-width')); ?>
 		<?php steed_before_primary_content(); ?>
         
 			<?php get_template_part( 'includes/content/tpl', 'page' ); ?>
